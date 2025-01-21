e0e046e3d7e626b2d292e4a6cc69ca1d0e055c7e
Saad Beidouri <saad.beidouri@etu.unice.fr>
2025-01-21 15:44:41 +0100

    swagger init

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
@@ -11,6 +12,14 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Backend')
+    .setDescription('The HeatPro API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
 
   try {
