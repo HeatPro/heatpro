e9c883d1f2edac7a6c613243077b1464c351207a
MichelK <michel.kadilar@etu.univ-cotedazur.fr>
2025-01-22 16:15:41 +0100

    updating custom button and refactoring validation modale

--- apps/mobile/app/(tabs)/modalValidation.tsx
+++ apps/mobile/app/(tabs)/modalValidation.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import { 
-  View, 
-  StyleSheet, 
-} from 'react-native';
-import PopUp from '@/components/ui/HeatProComponents/popUp';
+import { StyleSheet, View } from 'react-native';
+import ModaleComponent from '@/components/ui/HeatProComponents/ModaleComponent';
 
 const ModalValidation = () => {
-    return(
-        <View style = {styles.container}>
-          <PopUp></PopUp>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <ModaleComponent title={'Valider le formulaire ?'}
+                       description={'Êtes-vous sûr de vouloir valider le formulaire et terminer l’intervention en cours ?'}
+                       leftButtonText={'Annuler'} rightButtonText={'Valider'}></ModaleComponent>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container : {
-      backgroundColor: '#F4F5F8',
-      flex:1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
+  container: {
+    backgroundColor: '#F4F5F8',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
 
 export default ModalValidation;
