3a8e91af59507e3606f6e7e425e5c360a4e7f116
MichelK <michel.kadilar@etu.univ-cotedazur.fr>
2025-01-23 12:21:14 +0100

    basic routing ok

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,13 +1,20 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Stack, useNavigation } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { IPProvider } from './(tabs)/IPContext';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from '@/app/(tabs)/login';
+import HomePageScreen from '@/app/(tabs)/home-page';
+import ProfilePage from '@/app/(tabs)/profile-page';
+import InterventionListPage from '@/app/(tabs)/InterventionListPage';
+import InterventionPage from '@/app/(tabs)/InterventionPage';
+import { View } from 'react-native';
+import NavigationBar from '@/components/ui/HeatProComponents/NavigationBar';
+import { NavigationContainer, useNavigationState } from '@react-navigation/native';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -15,7 +22,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf')
   });
 
   useEffect(() => {
@@ -28,15 +35,47 @@ export default function RootLayout() {
     return null;
   }
 
+  const Stack = createNativeStackNavigator();
+
+
+
   return (
-    <IPProvider>
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="+not-found" />
-        </Stack>
-        <StatusBar style="auto" />
-      </ThemeProvider>
-    </IPProvider>
+      <IPProvider>
+        <View style={{ flex: 1 }}>
+          <Stack.Navigator
+            screenOptions={{
+              headerShown: false
+            }}
+          >
+            <Stack.Screen name="HomePage" component={HomePageScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Profile" component={ProfilePage} />
+            <Stack.Screen name="HistoriqueIntervention" component={InterventionListPage} />
+            <Stack.Screen name="DetailsIntervention" component={InterventionPage} />
+          </Stack.Navigator>
+
+          <BottomNavigationWrapper></BottomNavigationWrapper>
+        </View>
+      </IPProvider>
+  );
+}
+
+function getRouteName(state) {
+  if (state?.routes?.length > 0) {
+    return state.routes[state.index]?.name || 'Login';
+  }
+  return 'Login';
+}
+
+function BottomNavigationWrapper() {
+  const navigation = useNavigation();
+  const routeName = useNavigationState(getRouteName);
+
+  return (
+    <NavigationBar
+      navBarWidth="100%"
+      currentRouteName={routeName}
+      navigation={navigation}
+    />
   );
 }
